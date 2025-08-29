b8223b58e5424602d4a408434de49aafd04ba872
Alex <almm@itu.dk>
2025-08-29 14:13:40 +0200

    fixed pathing issues for the cvs file.
    det er muligt en del af løsningen skal implementeres lokalt på alled gruppe medlemmeres enheder.

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,33 +1,40 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Program {
-    private static List<string[]> messages;
-    private static void Main(string[] args) {
-        try{
-            if(args[0] == "read"){
-                ReadCsvFile("C:/Users/bruger/Desktop/Chirp/Chirp.CLI/data/chirp_cli_db.csv");
-                PostMessage();
-            }
-        } catch (Exception e){
-            throw new Exception("Missing argument."); 
+class Program
+{
+    private static List<string[]> messages = new();
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0) throw new ArgumentException("Missing argument.");
+
+        if (args[0] == "read")
+        {
+            var csvPath = Path.Combine(AppContext.BaseDirectory, "data", "chirp_cli_db.csv");
+            ReadCsvFile(csvPath);
+            PostMessage();
         }
     }
-    private static void ReadCsvFile(string filepath) {
-        List<string[]> lines = new List<string[]>();
-        foreach(var line in File.ReadLines(filepath)) {
-            string[] parts = line.Split(',');
-            if (parts[0] == "Author") continue; 
+
+    private static void ReadCsvFile(string filepath)
+    {
+        var lines = new List<string[]>();
+        foreach (var line in File.ReadLines(filepath))
+        {
+            var parts = line.Split(',');
+            if (parts.Length == 0 || parts[0] == "Author") continue;
             lines.Add(parts);
-        } 
-        messages = lines; 
+        }
+        messages = lines;
     }
 
-    private static void PostMessage() {
-        Console.WriteLine(messages[0][0]);
-    }
-    
-    private static string ConvertTime(string date) {
-        DateTimeOffset dateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(date)); 
-        return dateTime.UtcDateTime + "";
+    private static void PostMessage() => Console.WriteLine(messages[0][1]);
+
+    private static string ConvertTime(string date)
+    {
+        var ts = long.Parse(date);
+        return DateTimeOffset.FromUnixTimeSeconds(ts).UtcDateTime.ToString("o");
     }
 }
