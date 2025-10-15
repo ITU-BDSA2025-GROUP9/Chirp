ec41db03cc7e89e44bbba51b00f745b9b86d5142
cadalar <marcus.ryssel@gmail.com>
2025-10-15 16:04:15 +0200

    Hopefully final fux for Azure auto deployment workflow

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Chirp.Razor.DTO;
 using Chirp.Razor;
 
 namespace Chirp.Razor.Pages;
@@ -8,28 +7,33 @@ namespace Chirp.Razor.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
-    public required List<CheepViewModel> Cheeps { get; set; }
+
+    public required List<CheepViewModel> Cheeps { get; set; } = new();
 
     public PublicModel(ICheepService service)
     {
         _service = service;
     }
-    
+
     public ActionResult OnGet()
     {
         var pageQuery = Request.Query["page"];
         int pageno;
-    
-        if (!int.TryParse(pageQuery, out pageno) || pageno <= 0) {
+
+        if (!int.TryParse(pageQuery, out pageno) || pageno <= 0)
+        {
             pageno = 1;
         }
-        
-        try {
+
+        try
+        {
             Cheeps = _service.GetCheeps(pageno);
-        } catch (ArgumentOutOfRangeException) {
-            Cheeps = new List<CheepDTO>();
-        } 
-        
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Cheeps = new List<CheepViewModel>();
+        }
+
         return Page();
     }
 }
--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Chirp.Razor.DTO;
 using Chirp.Razor;
 
 namespace Chirp.Razor.Pages;
@@ -8,7 +7,8 @@ namespace Chirp.Razor.Pages;
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
-    public required List<CheepViewModel> Cheeps { get; set; }
+
+    public required List<CheepViewModel> Cheeps { get; set; } = new();
 
     public UserTimelineModel(ICheepService service)
     {
@@ -19,17 +19,21 @@ public class UserTimelineModel : PageModel
     {
         var pageQuery = Request.Query["page"];
         int pageno;
-        
-        if (!int.TryParse(pageQuery, out pageno) || pageno <= 0) {
+
+        if (!int.TryParse(pageQuery, out pageno) || pageno <= 0)
+        {
             pageno = 1;
         }
 
-        try {
+        try
+        {
             Cheeps = _service.GetCheepsFromAuthor(author, pageno);
-        } catch (ArgumentException) {
-            Cheeps = new List<CheepDTO>();
-        } 
-        
+        }
+        catch (ArgumentException)
+        {
+            Cheeps = new List<CheepViewModel>();
+        }
+
         return Page();
     }
 }
