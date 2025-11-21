74c54fab9001d27f25cbdd44d8d4304018f6199f
Isabella <iswa@itu.dk>
2025-11-21 12:00:49 +0100

    UserInformation page that displays name, email and cheep-dates.
    Note: We still need file-download-functionality, showing the user's email, and showing the followed users, and improving the styling
    Co-Authored-By: IDKINANN <228912272+IDKINANN@users.noreply.github.com>
    Co-Authored-By: Mikkel <57750089+TheDHare@users.noreply.github.com>
    #96

--- src/Chirp.Web/Pages/UserInformation.cshtml.cs
+++ src/Chirp.Web/Pages/UserInformation.cshtml.cs
@@ -1,11 +1,66 @@
+using Chirp.Core;
+using Chirp.Core.DTO;
+using Chirp.Core.Interfaces;
+using Chirp.Web.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
 public class UserInformation : PageModel
 {
-    public void OnGet()
+    private readonly ICheepService _service;
+    private readonly UserManager<Author> _userManager;
+    public required IEnumerable<CheepDTO> Cheeps { get; set; }
+    public bool HasNextPage { get; set; }
+    public readonly int pageSize = 5; 
+    public int CurrentPage; 
+    public string CurrentAuthor = string.Empty; 
+    
+    [BindProperty]
+    public LoginModel.InputModel Input { get; set; } = new();
+    
+    public UserInformation(ICheepService service, UserManager<Author> userManager)
     {
-        
+        _service = service;
+        _userManager = userManager;
     }
+    
+    public async Task<IActionResult> OnGetAsync(string author)
+    {
+        var pageQuery = Request.Query["page"];
+        int pageno;
+        
+        if (!int.TryParse(pageQuery, out pageno) || pageno <= 0) {
+            pageno = 1;
+        }
+        
+        CurrentPage = pageno;
+        CurrentAuthor = author;
+        
+        try
+        {
+            var user = await _userManager.GetUserAsync(User);
+            List<CheepDTO> cheepsList;
+            if (user != null && user.UserName == author)
+            {
+                cheepsList = await _service.GetUserTimelineCheeps(author, pageno, pageSize + 1);
+            }
+            else
+            {
+                cheepsList = await _service.GetCheepsByAuthor(author, pageno, pageSize + 1);
+            }
+
+            Cheeps = cheepsList.Take(pageSize);
+            HasNextPage = cheepsList.Count > pageSize;
+        }
+        catch (ArgumentException)
+        {
+            Cheeps = new List<CheepDTO>();
+        } 
+        
+        return Page();
+    }
+
 }
